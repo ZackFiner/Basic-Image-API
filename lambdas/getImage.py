b32eda8740410e332d4f9c610b0f0381aa969b59
ZackFiner <zackary.finer@yahoo.com>
2021-06-09 23:46:28 -0700

    init

---
+++ lambdas/getImage.py
@@ -0,0 +1,35 @@
+import json
+from PIL import Image
+import io
+import boto3
+import base64
+
+client = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    if "pathParameters" not in event or "image_id" not in event["pathParameters"]:
+        return {
+            "statusCode": 400,
+            'body': {
+                'error': "Could not parse request"
+            }
+        }
+
+    image_id = event['pathParameters']["image_id"]
+
+    file_like = io.BytesIO()
+    client.download_fileobj('zack-finers-image-bucket', "{id}.jpg".format(id=image_id), file_like)
+    file_like.seek(0)
+    b64_encoded = base64.b64encode(file_like.getvalue()).decode()
+
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Content-Type": "image/jpeg"
+        },
+        'body': b64_encoded,
+        "isBase64Encoded": True
+
+    }
---
+++ lambdas/uploadImage.py
@@ -0,0 +1,54 @@
+import json
+from PIL import Image, ImageOps
+import urllib.request as url
+import io
+import boto3
+import uuid
+
+client = boto3.client('s3')
+
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(event)
+    if 'body' not in event:
+        return {
+            'statusCode': 400,
+            'body': {
+                'error': 'Error parsing request body'
+            }
+        }
+    body = json.loads(event['body'])
+    print(body)
+    if 'img_url' not in body:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'No image URL in request'})
+        }
+    img_url = body['img_url']
+
+    fd = url.urlopen(img_url)  # create file descriptor for img
+    img_file = io.BytesIO(fd.read())  # read file as bytes
+    img = Image.open(img_file)  # open bytes file with PIL
+
+    # Image manipulation here
+    new_img = ImageOps.grayscale(img)
+
+    new_file_obj = io.BytesIO()
+    new_img.save(new_file_obj, "JPEG")
+    new_file_obj.seek(0)
+
+    image_id = str(uuid.uuid1())
+
+    client.upload_fileobj(new_file_obj, "zack-finers-image-bucket", "{img_id}.jpg".format(img_id=image_id))
+
+    return {
+        'statusCode': 201,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body': json.dumps({
+            'image_id': image_id
+        })
+
+    }
