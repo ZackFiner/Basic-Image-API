22305e27758d7cc9af8aacb5a60dd084983d4cda
atthematyo <johnlove1996@gmail.com>
2021-06-11 21:45:45 -0700

    Fixed zipping

--- terraform/lambda_component/main.tf
+++ terraform/lambda_component/main.tf
@@ -1,22 +1,11 @@
-/*
 data "archive_file" "lambda_payload" {
   type        = "zip"
   source_file = "${var.source_file_path}/${var.lambda_name}.py"
   output_path = "${path.module}/${var.lambda_name}.zip"
-}*/
-
-resource "aws_s3_bucket_object" "obj" {
-  bucket  = "${var.s3_bucket_name}-sourcecode"
-  key     = "${var.lambda_name}.py"
-  source  = "${var.source_file_path}/${var.lambda_name}.py"
-
-  etag    = filemd5("${var.source_file_path}/${var.lambda_name}.py")
 }
 
 resource "aws_lambda_function" "lambda" { // create a aws lambda function with the terraform handle test_lambda
-  //filename      = "${path.module}/${var.lambda_name}.zip"// select the zip file containing the code that implements the lambda that we created above
-  s3_bucket     = "${var.s3_bucket_name}-sourcecode" // or just use our source code s3
-  s3_key        = "${var.lambda_name}.py"
+  filename      = "${path.module}/${var.lambda_name}.zip"// select the zip file containing the code that implements the lambda that we created above
 
   function_name = "${var.lambda_name}-tf" // the name of actual lambda on AWS
   role          = var.lambda_role_arn // select a role for the lambda
@@ -24,8 +13,7 @@ resource "aws_lambda_function" "lambda" { // create a aws lambda function with t
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  #source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = data.archive_file.lambda_payload.output_base64sha256
 
   runtime = var.lambda_runtime
   layers = var.lambda_layers
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,6 @@ data "aws_caller_identity" "current" {
 
 }
 
-resource "aws_s3_bucket" "sourcecode" {
-  bucket = "${var.bucket_name}-sourcecode"
-  acl = "private"
-}
-
 resource "aws_s3_bucket" "b" { // create an S3 bucket with the resource name 'b'
   bucket = var.bucket_name     // name the bucket zackfiners-tf-test-bucket in AWS
   acl    = "private"           // make it private
